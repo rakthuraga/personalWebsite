d7b1b6a873e3286b668558c55e8f9c47762fe9d4
rakthuraga <thuraga@usc.edu>
2024-09-28 20:00:46 -0700

    adding new project

--- src/constants/index.js
+++ src/constants/index.js
@@ -263,7 +263,7 @@ const projects = [
     source_code_link: "https://github.com/rakthuraga/InfectaScan",
   },
   {
-    name: "Job Search",
+    name: "OpenDrone AI",
     description:
       "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on job preferences.",
     tags: [
